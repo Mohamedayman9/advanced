4123ee14a21da71be05b7caaff5a3d4fd162c99a
NouranAyman <44302646+NouranAyman@users.noreply.github.com>
2018-11-01 02:36:46 +0200

    ADV.

---
+++ Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Customer
+{
+    public int ID
+    {
+        get;
+        set;
+    }
+    public string FirstName;
+    public string LastName;
+
+    public string Email;
+    public bool IsMale;
+
+    public DateTime Birthdate;
+
+    public string PhoneNumber;
+
+    public Address address;
+
+    public bool IsValidEmail;
+
+    public DateTime CreatedDate;
+
+    public Customer()
+
+    }
+
+}
